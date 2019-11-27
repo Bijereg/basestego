2b360efd6df03c28147eeba1299698473a1ddd0f
Sergey Belousov <bijereg@gmail.com>
2019-11-28 01:37:39 +1000

    New version: 1.1
    Translated docstrings to English, added fast mode, code quality improved

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-import basestego
 
-setup(name='basestego',
-      version=basestego.__version__,
-      description='Steganography with base64',
-      author=basestego.__author__,
-      author_email='bijereg@gmail.com',
+setup(name="basestego",
+      version="1.1",
+      description="Steganography with base64",
+      author="Sergey Belousov",
+      author_email="bijereg@gmail.com",
       packages=find_packages(),
-      python_requires='>=3.6',
-      test_suite='tests')
+      python_requires=">=3.6",
+      test_suite="tests")
--- tests.py
+++ tests.py
@@ -1,16 +1,20 @@
 from unittest import TestCase
-from base64 import b64decode
-from basestego import base64enc_inserted, base64dec_inserted
+from base64 import b64decode as orig_decode
+from basestego import b64encode, b64decode
 
 
-# TODO: Make more tests
 class BasestegoTestCase(TestCase):
-    def test_base64enc_inserted(self):
+    def test_b64encode_fast(self):
         for i in range(2**4):
-            test_string = base64enc_inserted(b'bijereg', i)
-            self.assertEqual(b'bijereg', b64decode(test_string))
+            test_string = b64encode(b"bijereg", i, False)
+            self.assertEqual(b"bijereg", orig_decode(test_string))
 
-    def test_base64dec_inserted(self):
+    def test_b64encode_slow(self):
         for i in range(2**4):
-            test_string = base64enc_inserted(b'bijereg', i)
-            self.assertEqual(int(base64dec_inserted(test_string), 2), i)
+            test_string = b64encode(b"bijereg", i, True)
+            self.assertEqual(b"bijereg", orig_decode(test_string))
+
+    def test_b64decode(self):
+        for i in range(2**4):
+            test_string = b64encode(b"bijereg", i)
+            self.assertEqual(b64decode(test_string), i)
