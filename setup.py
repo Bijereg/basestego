421f4facd8a0c435ff220381a909277a0d79a44d
Sergey Belousov <bijereg@gmail.com>
2019-11-28 02:02:26 +1000

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="basestego",
-      version="1.1",
+      version="1.1a",
       description="Steganography with base64",
       author="Sergey Belousov",
       author_email="bijereg@gmail.com",
